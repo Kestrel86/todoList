698d8b48d875be150a213a94bc00eb52aada40ec
Kestrel86 <valdezandrew20@gmail.com>
2023-01-08 16:00:14 -0800

    complete project

--- main.js
+++ main.js
@@ -1,63 +1,70 @@
 const input = document.getElementById("addInput");
+const button = document.getElementById("addButton");
 const taskContainer = document.getElementById("taskContainer");
 
-function addTask(content) {
-  //entire function for adding and removing tasks as well as strikethrough
-  const list = document.createElement("li"); //Creates list item in html
-  const contentLabel = document.createElement("span"); //label that is attached to leftContent div
-  const leftContent = document.createElement("div"); //division for the left side of content
-  const rightContent = document.createElement("div"); //division for the right side of content
+button.addEventListener("click", addButton);
 
-  //Creates checkBox attributes and properties
-  const checkBox = document.createElement("input"); //type of element
-  checkBox.setAttribute("type", "checkBox");
+function addTask(content) 
+{
+  const leftContainer = document.createElement("div");
+  const rightContainer = document.createElement("div");
+  const list = document.createElement("li");
+  const textBox = document.createElement("span");
+
+  const checkBox = document.createElement("input");
+  //checkBox.type = "checkbox";
+  checkBox.setAttribute("type", "checkbox");
   checkBox.style.cursor = "pointer";
 
-  //Creates removeButton attributes and properties
   const removeButton = document.createElement("button");
   removeButton.classList.add("removeButton");
   removeButton.innerText = "X";
   removeButton.style.cursor = "pointer";
 
-  contentLabel.innerText = content;
+  textBox.innerText = content;
 
-  checkBox.addEventListener("click", () => {
-    if (!contentLabel.classList.contains("strikethrough")) {
-      contentLabel.classList.add("strikethrough");
-    } else {
-      contentLabel.classList.remove("strikethrough");
-    }
-  });
+  checkBox.addEventListener("click", strikeCheckBox);
 
-  removeButton.addEventListener("click", () => {
-    taskContainer.removeChild(list);
-    input.focus();
-  });
+  removeButton.addEventListener("click", removeItem);
 
-  leftContent.appendChild(checkBox);
-  leftContent.appendChild(contentLabel);
+  leftContainer.appendChild(checkBox);
+  leftContainer.appendChild(textBox);
+  rightContainer.appendChild(removeButton);
 
-  rightContent.appendChild(removeButton);
-
-  list.appendChild(leftContent);
-  list.appendChild(rightContent);
+  list.appendChild(leftContainer);
+  list.appendChild(rightContainer);
 
   taskContainer.appendChild(list);
+
+  function strikeCheckBox() 
+  {
+   if(!textBox.classList.contains("strikethrough")) {
+    textBox.classList.add("strikethrough");
+   } else if (textBox.classList.contains("strikethrough")) {
+    textBox.classList.remove("strikethrough");
+   }
+  }
+
+  function removeItem() 
+  {
+    taskContainer.removeChild(list);
+    input.focus();
+  }
+
 }
 
-const button = document.getElementById("addButton");
-
-button.addEventListener("click", (event) => {
-  const content = input.value;
+function addButton(event)
+{
   event.preventDefault();
 
-  if (content.length == 0) {
-    alert("Task field must not be empty");
+  const userInput = input.value;
+  if(userInput.length == 0) 
+  {
+    alert("No Input Given");
     return;
   }
 
-  addTask(content);
-
-  // Clear the input's current value
+  addTask(userInput);
   input.value = "";
-});
+}
+
